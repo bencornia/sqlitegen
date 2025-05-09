604dccd81e119a945019e3498a1f4ee576666111
bencornia <ben.cornia@gmail.com>
2025-05-08 21:07:35 -0600

    use writer instead filename

--- cmd/sqlitegen/main.go
+++ cmd/sqlitegen/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "github.com/bencornia/sqlitegen/pkg/codegen"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/bencornia/sqlitegen/pkg/codegen"
+)
 
 func main() {
-	codegen.Generate("foo.db", "bar")
+	outFile := flag.String("output", "", "Optional output file name")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "Usage %s <inputfile> [-output <outputfile>]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	var writer io.Writer = os.Stdout
+	if *outFile != "" {
+		file, err := os.Create(*outFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create output file: %v\n", err)
+			os.Exit(1)
+		}
+
+		defer func(file *os.File) {
+			err := file.Close()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "An unknown error occurred while closing the output file: %v\n", err)
+				os.Exit(1)
+			}
+		}(file)
+
+		writer = file
+	}
+
+	codegen.Generate(args[0], writer)
 }
--- pkg/codegen/generate.go
+++ pkg/codegen/generate.go
@@ -3,7 +3,8 @@ package codegen
 import (
 	"database/sql"
 	"fmt"
-	"log"
+	"io"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -19,47 +20,50 @@ type column struct {
 	NotNull bool
 }
 
-func Generate(dbPath string, outFile string) {
-	db := newDB(dbPath)
+func catch(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
+
+func Generate(dsn string, writer io.Writer) {
+	db, err := sql.Open("sqlite3", dsn)
+	catch(err)
 
 	var exists bool
-	err := db.QueryRow("select count(*) > 0 from sqlite_master").Scan(&exists)
-	if err != nil {
-		log.Fatal(err)
-	}
+	err = db.QueryRow("select count(*) > 0 from sqlite_master").Scan(&exists)
+	catch(err)
 
 	if !exists {
-		log.Fatal("No tables!")
+		catch(fmt.Errorf("No tables in %s", dsn))
 	}
 
-	// type|name|tbl_name|rootpage|sql
 	tableNames, err := db.Query("select name from sqlite_master where type = 'table'")
-	if err != nil {
-		log.Fatal(err)
-	}
+	catch(err)
 
 	var schemas []*schema
-	defer closeRows(tableNames)
+	defer func(rows *sql.Rows) {
+		catch(rows.Close())
+	}(tableNames)
+
 	for tableNames.Next() {
 		var tableName string
 		err = tableNames.Scan(&tableName)
-		if err != nil {
-			log.Fatal(err)
-		}
+		catch(err)
 
 		c, err := db.Query("select name, type, `notnull` from pragma_table_info('?')", tableName)
-		if err != nil {
-			log.Fatal(err)
-		}
+		catch(err)
 
-		defer closeRows(c)
+		defer func(rows *sql.Rows) {
+			catch(rows.Close())
+		}(c)
+
 		s := &schema{Name: tableName, Columns: []*column{}}
 		for c.Next() {
 			var col column
 			err = c.Scan(&col.Name, &col.Type, &col.NotNull)
-			if err != nil {
-				log.Fatal(err)
-			}
+			catch(err)
 
 			s.Columns = append(s.Columns, &col)
 		}
@@ -67,21 +71,6 @@ func Generate(dbPath string, outFile string) {
 		schemas = append(schemas, s)
 	}
 
-	fmt.Println(schemas)
-}
-
-func newDB(dsn string) *sql.DB {
-	db, err := sql.Open("sqlite3", dsn)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return db
-}
-
-func closeRows(rows *sql.Rows) {
-	err := rows.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
+	_, err = writer.Write([]byte(fmt.Sprintf("%v\n", schemas)))
+	catch(err)
 }
