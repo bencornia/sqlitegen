6eea6920fb267e7571c6008f2e53f2879525e762
bencornia <ben.cornia@gmail.com>
2025-05-12 23:45:41 -0600

    Add to tmpl

--- pkg/codegen/generate.go
+++ pkg/codegen/generate.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"text/template"
+	"unicode"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -39,7 +40,7 @@ func Generate(dsn string, writer io.Writer) {
 	catch(err)
 
 	if !exists {
-		catch(fmt.Errorf("No tables in %s", dsn))
+		catch(fmt.Errorf("no tables in %s", dsn))
 	}
 
 	// Step 3) Get schemas
@@ -56,17 +57,18 @@ func Generate(dsn string, writer io.Writer) {
 		err = tableNames.Scan(&tableName)
 		catch(err)
 
-		c, err := db.Query("select name, type, `notnull` from pragma_table_info('?')", tableName)
+		query := fmt.Sprintf("select name, type, `notnull` from pragma_table_info('%s')", tableName)
+		columns, err := db.Query(query)
 		catch(err)
 
 		defer func(rows *sql.Rows) {
 			catch(rows.Close())
-		}(c)
+		}(columns)
 
 		s := &schema{Name: tableName, Columns: []*column{}}
-		for c.Next() {
+		for columns.Next() {
 			var col column
-			err = c.Scan(&col.Name, &col.Type, &col.NotNull)
+			err = columns.Scan(&col.Name, &col.Type, &col.NotNull)
 			catch(err)
 
 			s.Columns = append(s.Columns, &col)
@@ -75,8 +77,20 @@ func Generate(dsn string, writer io.Writer) {
 		schemas = append(schemas, s)
 	}
 
-	// Step 4) Execute template
-	tmpl := template.Must(template.New("").Parse(genTmpl))
+	// Step 4) Register template functions
+	funcs := template.FuncMap{
+		"title": func(val string) string {
+			runes := []rune(val)
+			runes[0] = unicode.ToUpper(runes[0])
+			return string(runes)
+		},
+		"jsonTag": func(col *column) string {
+			return fmt.Sprintf("`json:\"%s\"`", col.Name)
+		},
+	}
+
+	// Step 5) Execute template
+	tmpl := template.Must(template.New("").Funcs(funcs).Parse(genTmpl))
 	err = tmpl.Execute(writer, schemas)
 	catch(err)
 }
@@ -85,6 +99,26 @@ var genTmpl = `
 package models
 
 {{ range . }}
+	type {{ .Name | title }} struct {
+		{{ range .Columns }}
+			{{ .Name | title }} {{ . | jsonTag }}
+		{{ end }}
+	}
 
-{{ end }}
+	func GetAll{{ .Name | title }}() ([]*{{ .Name | title }}, error) {
+		
+	}
+
+	func Get{{ .Name | title }}(id int) (*{{ .Name | title }}, error) {
+		
+	}
+
+	func Update{{ .Name | title }}({{ .Name }} *{{ .Name | title }}) (*{{ .Name | title }}, error) {
+		
+	}
+
+	func Delete{{ .Name | title }}(id int) (error) {
+		
+	}
+{{ end -}}
 	`
